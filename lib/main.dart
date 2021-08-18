6f3d73f3a78840b91638c4ef1960d6310e10f4aa
Lakshmi Jayasri G <lakshlj816@gmail.com>
2021-08-18 16:01:53 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -26,6 +26,10 @@ class _MyAppState extends State<MyApp> {
   String _value = "";
   bool? _val1 = false;
   bool? _val2 = false;
+  int? _value1 = 0;
+  int? _value2 = 0;
+  bool _v1 = false;
+  bool _v2 = false;
 
   void _onChange(String value) {
     setState(() {
@@ -51,42 +55,166 @@ class _MyAppState extends State<MyApp> {
     });
   }
 
+  void _value1RadioChanged(int? value) {
+    setState(() {
+      _value1 = value;
+    });
+  }
+
+  void _value2RadioChanged(int? value) {
+    setState(() {
+      _value2 = value;
+    });
+  }
+
+  void _value1SwitchChanged(bool value) {
+    setState(() {
+      _v1 = value;
+    });
+  }
+
+  void _value2SwitchChanged(bool value) {
+    setState(() {
+      _v2 = value;
+    });
+  }
+
+  Widget makeRadios() {
+    List<Widget> list = [];
+    for (int i = 0; i < 3; i++) {
+      list.add(
+          Radio(value: i, groupValue: _value1, onChanged: _value1RadioChanged));
+    }
+    Column column = Column(
+      children: list,
+    );
+    return column;
+  }
+
+  Widget makeRadioListTiles() {
+    List<Widget> list = [];
+    for (int i = 0; i < 3; i++) {
+      list.add(
+        RadioListTile(
+          value: i,
+          groupValue: _value2,
+          onChanged: _value2RadioChanged,
+          title: Text("Item $i"),
+          activeColor: Colors.purple,
+          controlAffinity: ListTileControlAffinity.trailing,
+          secondary: Icon(Icons.radio),
+          subtitle: Text("Subtitle $i"),
+        ),
+      );
+    }
+    Column column = Column(
+      children: list,
+    );
+    return column;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Container(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                _value,
-              ),
-              TextField(
-                decoration: InputDecoration(
-                  labelText: "Email",
-                  hintText: "Enter email",
-                  icon: Icon(Icons.email),
+      body: SingleChildScrollView(
+        child: Container(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  _value,
                 ),
-                onChanged: _onChange,
-                onSubmitted: _onSubmit,
-                autofocus: true,
-                keyboardType: TextInputType.emailAddress,
-              ),
-              Checkbox(value: _val1, onChanged: _value1Changed),
-              CheckboxListTile(
-                value: _val2,
-                onChanged: _value2Changed,
-                activeColor: Colors.blue,
-                title: Text("CheckBoxListTile"),
-                subtitle: Text("SubTitle - checkbox"),
-                controlAffinity: ListTileControlAffinity.leading,
-                secondary: Icon(Icons.archive),
-              )
-            ],
+                Text(
+                  "Text Field",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.teal,
+                  ),
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                    labelText: "Email",
+                    hintText: "Enter email",
+                    icon: Icon(Icons.email),
+                  ),
+                  onChanged: _onChange,
+                  onSubmitted: _onSubmit,
+                  autofocus: true,
+                  keyboardType: TextInputType.emailAddress,
+                ),
+                Text(
+                  "CheckBox",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.teal,
+                  ),
+                ),
+                Checkbox(value: _val1, onChanged: _value1Changed),
+                Text(
+                  "CheckboxListTile",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.teal,
+                  ),
+                ),
+                CheckboxListTile(
+                  value: _val2,
+                  onChanged: _value2Changed,
+                  activeColor: Colors.purple,
+                  title: Text("CheckBoxListTile"),
+                  subtitle: Text("SubTitle - checkbox"),
+                  controlAffinity: ListTileControlAffinity.leading,
+                  secondary: Icon(Icons.archive),
+                ),
+                Text(
+                  "Radio Buttons",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.teal,
+                  ),
+                ),
+                makeRadios(),
+                Text(
+                  "Radio Buttons List Tile",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.teal,
+                  ),
+                ),
+                makeRadioListTiles(),
+                Text(
+                  "Switch",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.teal,
+                  ),
+                ),
+                Switch(value: _v1, onChanged: _value1SwitchChanged),
+                Text(
+                  "Switch List Tile",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.teal,
+                  ),
+                ),
+                SwitchListTile(
+                  value: _v2,
+                  onChanged: _value2SwitchChanged,
+                  controlAffinity: ListTileControlAffinity.trailing,
+                  secondary: Icon(Icons.switch_account),
+                  subtitle: Text("Sub Title"),
+                  activeColor: Colors.purple,
+                  title: Text(
+                    "Switch List Tile",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
